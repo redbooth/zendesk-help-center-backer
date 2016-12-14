1374590eb4469b3943c135b1e106b61754a14a17
Suthan Nandakumar <suthan@aerofs.com>
2016-12-14 11:40:17 -0800

    Restructure for pypi submission
    updated readme
    
    add local path to articles directory
    
    Remove articles directory dependency

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+    requirements = [x for x in requirements if not x.startswith('#')]
+
+setuptools.setup(   
+    name='zendesk-help-center-backer',
+    version='0.1',
+    description='A Zendesk help center backer written in Python',
+    long_description='A Zendesk help center backer written in Python',
+    keywords='zendesk,solution,help center,knowledge base',
+    author='AeroFS',
+    author_email='oss@aerofs.com',
+    url='https://github.com/aerofs/zendesk-help-center-backer',
+    packages=['zendesk', 'zendesk.scripts'],
+    entry_points={
+        'console_scripts': [
+            'zendesk-new = zendesk.create_new_post_shell:main',
+            'zendesk-track = zendesk.track_changes:main',
+            'zendesk-deploy = zendesk.deploy:main',
+        ]
+    },
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+)
